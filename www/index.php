4c1efeba6e39eda2a38d0eb76f07bd280875d2f7
Ksenia Maslennikova <loiste@yandex.ru>
2016-03-15 19:22:23 +0300

    Added class Articles

--- www/index.php
+++ www/index.php
@@ -6,4 +6,22 @@
  * Time: 14:29
  */
 
-phpinfo();
+class Article {
+    public $title;
+    public $text;
+
+    public function __construct($title, $text){
+        $this->title=$title;
+        $this->text=$text;
+    }
+
+    public function view(){
+        echo $this->title;
+    }
+}
+
+$a = new Article('Заголовок новости','Текст новости');
+$a->view();
+
+$b=new Article('Другой заголовок','Текст');
+$b->view();
